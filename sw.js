6e0850d467b9ea687df53c94732215a9f0f07a25
rkrishnan2012 <rkrishnan2012@users.noreply.github.com>
2025-11-02 22:34:54 +0000

    deploy: 683696d903f4fe914fa5faff5530f6171e26f9f6

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'dwellable-v1';
+const urlsToCache = [
+  '/',
+  '/index.min.html',
+  '/home.png',
+  '/reminders.png', 
+  '/property_info.png',
+  '/inspection.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
